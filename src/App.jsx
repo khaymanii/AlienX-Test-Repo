1d8715ebd84b74bccfdfd006af12d4dd40d35ed8
khaymanii <kennethakpo61@gmail.com>
2025-03-31 12:16:17 +0100

    Remove Material Icons link, update Button component layout, and add MUI icons dependency

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import Button from "./components/Button";
 
 function App() {
   return (
-    <>
+    <div className="flex items-center justify-center h-screen">
       <Button />
-    </>
+    </div>
   );
 }
 
--- src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Button() {
-  return (
-    <div className="flex justify-center items-center h-screen">Button</div>
-  );
-}
-
-export default Button;
